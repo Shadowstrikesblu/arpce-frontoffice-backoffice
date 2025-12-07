a4033d7d6b133b9dbafb1525832bf59a2b25cc13
Oriche Kaya <orichekaya1@gmail.com>
2025-12-07 03:50:04 +0100

    La nouvelle version avec paiement du back office

--- arpce-homologation-backOffice/Arpce.Homologation.BackOffice/BackOffice.Infrastructure/Persistence/Configurations/StatutConfiguration.cs
+++ arpce-homologation-backOffice/Arpce.Homologation.BackOffice/BackOffice.Infrastructure/Persistence/Configurations/StatutConfiguration.cs
@@ -27,22 +27,23 @@ public class StatutConfiguration : IEntityTypeConfiguration<Statut>
             .IsRequired();
 
         builder.HasData(
-            new Statut { Id = Guid.NewGuid(), Code = "NouveauDossier", Libelle = "Nouvelle demande" },
-            new Statut { Id = Guid.NewGuid(), Code = "RefusDossier", Libelle = "Refus de la demande" },
-            new Statut { Id = Guid.NewGuid(), Code = "Instruction", Libelle = "En cours d'instruction" },
-            new Statut { Id = Guid.NewGuid(), Code = "ApprobationInstruction", Libelle = "Envoyé pour approbation" },
-            new Statut { Id = Guid.NewGuid(), Code = "InstructionApprouve", Libelle = "Instruction Approuvée" },
-            new Statut { Id = Guid.NewGuid(), Code = "DevisCreer", Libelle = "Devis créé" },
-            new Statut { Id = Guid.NewGuid(), Code = "DevisValideSC", Libelle = "Devis validé par Chef Service" },
-            new Statut { Id = Guid.NewGuid(), Code = "DevisValideTr", Libelle = "Devis validé par Trésorerie" },
-            new Statut { Id = Guid.NewGuid(), Code = "DevisEmit", Libelle = "Devis émis" },
-            new Statut { Id = Guid.NewGuid(), Code = "DevisValide", Libelle = "Devis validé par client" },
-            new Statut { Id = Guid.NewGuid(), Code = "DevisRefuser", Libelle = "Devis refusé par client" },
-            new Statut { Id = Guid.NewGuid(), Code = "PaiementRejete", Libelle = "Paiement non accepté" },
-            new Statut { Id = Guid.NewGuid(), Code = "PaiementExpirer", Libelle = "Paiement expiré" },
-            new Statut { Id = Guid.NewGuid(), Code = "DossierPayer", Libelle = "Paiement effectué" },
-            new Statut { Id = Guid.NewGuid(), Code = "DossierSignature", Libelle = "Attestation en signature" },
-            new Statut { Id = Guid.NewGuid(), Code = "DossierSigner", Libelle = "Attestation signée" }
-        );
+             new Statut { Id = Guid.NewGuid(), Code = "NouveauDossier", Libelle = "Nouvelle demande" },
+             new Statut { Id = Guid.NewGuid(), Code = "RefusDossier", Libelle = "Refus de la demande" },
+             new Statut { Id = Guid.NewGuid(), Code = "Instruction", Libelle = "En cours d'instruction" },
+             new Statut { Id = Guid.NewGuid(), Code = "ApprobationInstruction", Libelle = "Envoyé pour approbation" },
+             new Statut { Id = Guid.NewGuid(), Code = "InstructionApprouve", Libelle = "Instruction Approuvée" },
+             new Statut { Id = Guid.NewGuid(), Code = "DevisCreer", Libelle = "Devis créé" },
+             new Statut { Id = Guid.NewGuid(), Code = "DevisValideSC", Libelle = "Devis validé par Chef Service" },
+             new Statut { Id = Guid.NewGuid(), Code = "DevisValideTr", Libelle = "Devis validé par Trésorerie" },
+             new Statut { Id = Guid.NewGuid(), Code = "DevisEmit", Libelle = "Devis émis" },
+             new Statut { Id = Guid.NewGuid(), Code = "DevisValide", Libelle = "Devis validé par client" },
+             new Statut { Id = Guid.NewGuid(), Code = "DevisRefuser", Libelle = "Devis refusé par client" },
+             new Statut { Id = Guid.NewGuid(), Code = "DevisPaiement", Libelle = "En attente de paiement" },
+             new Statut { Id = Guid.NewGuid(), Code = "PaiementRejete", Libelle = "Paiement non accepté" },
+             new Statut { Id = Guid.NewGuid(), Code = "PaiementExpirer", Libelle = "Paiement expiré" },
+             new Statut { Id = Guid.NewGuid(), Code = "DossierPayer", Libelle = "Paiement effectué" },
+             new Statut { Id = Guid.NewGuid(), Code = "DossierSignature", Libelle = "Attestation en signature" },
+             new Statut { Id = Guid.NewGuid(), Code = "DossierSigner", Libelle = "Attestation signée" }
+         );
     }
 }
