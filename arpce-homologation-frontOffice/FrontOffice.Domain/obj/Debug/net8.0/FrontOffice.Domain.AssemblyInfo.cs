05837058b8c2b9db7d4a1c73e89137851d5bc5c0
Oriche Kaya <orichekaya1@gmail.com>
2026-01-16 11:34:57 +0100

    mis à jour pour le déploiement à l'ARPCE

--- arpce-homologation-frontOffice/FrontOffice.Domain/obj/Debug/net8.0/FrontOffice.Domain.AssemblyInfo.cs
+++ arpce-homologation-frontOffice/FrontOffice.Domain/obj/Debug/net8.0/FrontOffice.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontOffice.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aed3c31c356ce60b4dfc8f7f68abdb6868877888")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a5cdee3f85fc01d09024186c7ca6f9dfb346722")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontOffice.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontOffice.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
