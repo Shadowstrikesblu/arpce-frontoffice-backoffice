5cf33e547d300b2b440befdcf5a58dc9ce4dc6a0
Oriche Kaya <orichekaya1@gmail.com>
2026-02-10 13:45:52 +0100

    Mise à jour 2026 1

--- arpce-homologation-frontOffice/FrontOffice.Api/obj/Debug/net8.0/FrontOffice.Api.AssemblyInfo.cs
+++ arpce-homologation-frontOffice/FrontOffice.Api/obj/Debug/net8.0/FrontOffice.Api.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontOffice.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd728f59aedba4f997d6c4e0500ce616306a175e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65465540af5745090d8656452ece4b69c02e6290")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontOffice.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontOffice.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
